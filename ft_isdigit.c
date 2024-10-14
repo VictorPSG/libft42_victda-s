ca5640b04d2bc90f40005c6df27db65738f57479
Victor Paulo Da Silva Garcia <victda-s@c2r9p6.42sp.org.br>
2024-10-14 18:29:17 -0300

    a

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: victda-s <victda-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 18:18:17 by victda-s          #+#    #+#             */
-/*   Updated: 2024/10/14 18:27:20 by victda-s         ###   ########.fr       */
+/*   Updated: 2024/10/14 18:29:07 by victda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,10 @@ int	ft_isdigit(int c)
 	else
 		return (0);
 }
-
+/*
 #include <stdio.h>
 int	main(void)
 {
 	printf("Resultado %d", ft_isdigit('a'));
 }
+*/
