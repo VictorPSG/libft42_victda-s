415ac14b19f70e87c7ca4cf42b7376c1ea5aaa14
Victor Paulo Da Silva Garcia <victda-s@c3r12p1.42sp.org.br>
2024-10-18 16:29:03 -0300

    atoi fora da norma

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,53 +6,65 @@
 /*   By: victda-s <victda-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 11:15:51 by victda-s          #+#    #+#             */
-/*   Updated: 2024/10/18 15:24:47 by victda-s         ###   ########.fr       */
+/*   Updated: 2024/10/18 16:28:11 by victda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+static int	count_sign(const unsigned char *str)
+{
+	int	i;
+	int	sign;
+
+	i = 0;
+	sign = 0;
+	while (!(ft_isdigit(str[i])) && str[i] != '\0')  // Evitar loop infinito
+	{
+		if (str[i] == '-' || str[i] == '+')
+			sign++;
+		i++;
+	}
+	return (sign);
+}
 
 int	ft_atoi(const char *nptr)
 {
-	int	i;
-	int	toNum;
-	int	flag;
+	int		i;
+	int		toNum;
+	int		flag;
 	const unsigned char *str = (const unsigned char *)nptr;
 	
 	i = 0;
 	toNum = 0;
 	flag = 0;
-	while((str[i++] && (ft_isdigit(str[i]))) || (str[i] == ' ' && flag == 0) || str[i] == '-' || str[i] == '+')
+	if (count_sign(str) > 1)
+		return (0);
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
 	{
-		while(ft_isdigit(str[i]))
-		{
-			if(flag == 0)
-			{
-				flag++;
-				if(str[i - 1] == '-')
-					flag++;
-				toNum = str[i++] - '0';
-			}
-			else
-				toNum = (toNum * 10) + (str[i++] - '0');
-			if(!(ft_isdigit(str[i])))
-				return toNum * (1 - (flag == 2) * 2);
-		}
+		if (str[i] == '-')
+			flag = 1;
+		i++;
 	}
-	return toNum * (1 - (flag == 2) * 2);
+	while (ft_isdigit(str[i]))
+	{
+		toNum = (toNum * 10) + (str[i] - '0');
+		i++;
+	}
+	if (flag == 1)
+		toNum = -toNum;
+	return toNum (flag == 1) ;
 }
 /*
  #include <stdlib.h>
 int	main(void)
 {
-	const char teste[] = "--54a8";
+	const char teste[] = "054854";
 
 	printf("resultado: %d", atoi(teste));
 	printf("\nresultado ft: %d  \n", ft_atoi(teste));
 }
 */
-
-
-
 
 
