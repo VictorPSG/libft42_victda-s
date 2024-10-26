dd0ca11091e09923c9b40ff6d54ccafc73e27bbe
Victor Paulo Da Silva Garcia <victda-s@c1r6p2.42sp.org.br>
2024-10-26 18:16:59 -0300

    conclusao ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,43 +6,37 @@
 /*   By: victda-s <victda-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 23:22:55 by victda-s          #+#    #+#             */
-/*   Updated: 2024/10/26 17:10:45 by victda-s         ###   ########.fr       */
+/*   Updated: 2024/10/26 18:16:28 by victda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int		i;
-	int		i2;
+	int		start;
+	int		end;
 	int		index_arr;
 	char	*arr;
 
-	i = 0;
-	i2 = ft_strlen(s1);
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	end = ft_strlen(s1);
 	index_arr = 0;
-	while (s1[i] && ft_strchr(set, s1[i]))
-		i++;
-	while (s1[i2 - 1] && ft_strchr(set, s1[i2 - 1]))
-		i2--;
-	arr = (char *)malloc((i2 - i) + 1);
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	arr = (char *)malloc((end - start + 1) * sizeof(char));
 	if (!arr)
 		return (NULL);
-	printf("i: %s\n", ft_strchr(set, s1[i2 - i]));
-	printf("i2: %d\n", i2);
-	while (i < i2)
-	{
-		arr[index_arr] = s1[i];
-		i++;
-		index_arr++;
-	}
+	while (start < end)
+		arr[index_arr++] = s1[start++];
 	arr[index_arr] = '\0';
 	return (arr);
 }
-
-int	main(void)
-{
-	printf("retorno:%s", ft_strtrim(" lorem ipsum dolor sit amet", "l"));
-}
+// int	main(void)
+// {
+// 	printf("retorno:%s", ft_strtrim(" lorem ipsum dolor sit amet", "l"));
+// }
