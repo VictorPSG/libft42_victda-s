8b4b4c8ae78a25163b36ee63e9497209b8243146
Victor Paulo Da Silva Garcia <victda-s@c3r10p2.42sp.org.br>
2024-10-29 18:14:39 -0300

    correcao NULL memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: victda-s <victda-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: victda-s <victda-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 22:34:50 by victda-s          #+#    #+#             */
-/*   Updated: 2024/10/29 10:48:49 by victda-s         ###   ########.fr       */
+/*   Updated: 2024/10/29 18:12:14 by victda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
+	if(!src && !dest)
+		return (NULL);
 	while (i < n)
 	{
 		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
