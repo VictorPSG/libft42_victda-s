6ab477dccd80f8d0045c7976f840e207756190fe
Victor Paulo Da Silva Garcia <victda-s@c3r10p2.42sp.org.br>
2024-10-29 16:10:32 -0300

    correcao retorno null memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: victor <victor@student.42.fr>              +#+  +:+       +#+        */
+/*   By: victda-s <victda-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 19:35:54 by victda-s          #+#    #+#             */
-/*   Updated: 2024/10/29 01:48:19 by victor           ###   ########.fr       */
+/*   Updated: 2024/10/29 16:07:22 by victda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	size_t	lenght;
 
 	i = 0;
-	lenght = ft_strlen(s) - 1;
+	lenght = ft_strlen(s);
 	if (c == '\0')
 		return ((char *)s + lenght);
 	while (i < n && ((unsigned char *) s)[i] != (unsigned char) c)
@@ -30,23 +30,24 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	return ((void *)((unsigned char *)s + i));
 
 }
-/*
-#include <stdio.h>
 
-int main(void)
-{
-    const char str[] = "bonjourno";
-    char *result;
-    char ch = '';
-    size_t n = 2;  // Tamanho da string sem o terminador nulo
+// #include <stdio.h>
+// #include <string.h>
+// int main(void)
+// {
+//     const char str[] = "/|\x12\xff\x09\0\x42\042\0\42|\\";
+//     char *result;
+//     char ch = '\0';
+//     size_t n = 12;  // Tamanho da string sem o terminador nulo
 
-    // Usando ft_memchr para procurar o caractere 'o'
-    result = (char *)ft_memchr(str, ch, n);
-    if (result != NULL)
-        printf("ft_memchr: Caractere '%c' encontrado: %s\n", ch, result);
-    else
-        printf("ft_memchr: Caractere '%c' não encontrado.\n", ch);
-
-    return 0;
-}
-*/
+//     // Usando ft_memchr para procurar o caractere 'o'
+//     result = (char *)ft_memchr(str, ch, n);
+// 	if(ft_memchr(str, ch, n) == memchr(str, ch, n))
+// 		printf("são iguais");
+// 	else
+// 	{
+// 		printf("mem: %s\n", (char *)memchr(str, ch, n));
+// 		printf("ft: %s\n", (char *)ft_memchr(str, ch, n));
+// 	}
+//     return 0;
+// }
